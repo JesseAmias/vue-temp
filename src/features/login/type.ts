c2ed7c92c537839b76dab1722e57c767603a1f7a
郭高翔 <guogaoxiang@travelsky.com.cn>
2024-09-30 16:26:04 +0800

    init

---
+++ src/features/login/type.ts
@@ -0,0 +1,31 @@
+// Start of Selection
+export interface LoginCredentials {
+  username: string
+  password: string
+  captchaToken: string // 添加滑动校验的令牌
+}
+
+export interface LoginResponse {
+  token: string
+  expiresIn: number
+  refreshToken: string // 添加刷新令牌
+}
+
+export interface LoginState {
+  isLogin: boolean
+  user?: {
+    id: string
+    username: string
+  }
+  captchaRequired: boolean // 是否需要滑动校验
+}
+
+export interface ErrorType {
+  code: string
+  msg: string
+  errorCodeHigherLevel: string
+  errorMsgHigherLevel: string
+  originErrorCode: string
+  originErrorMsg: string
+  appVersion: string
+}
---
+++ src/lib/axios.ts
@@ -0,0 +1,50 @@
+import Axios, { AxiosError } from 'axios'
+export const HTTP_STATUS = {
+  SUCCESS: 200,
+  CREATED: 201,
+  ACCEPTED: 202,
+  CLIENT_ERROR: 400,
+  AUTHENTICATE: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  SERVER_ERROR: 500,
+  BAD_GATEWAY: 502,
+  SERVICE_UNAVAILABLE: 503,
+  GATEWAY_TIMEOUT: 504
+}
+
+const handleHttpCode = (error: AxiosError<{ code: string; msg: string }>) => {
+  //const code = error.response?.status;
+  return Promise.reject(error)
+  // switch (code) {
+  //   case HTTP_STATUS.AUTHENTICATE: {
+  //     return Promise.reject(error);
+  //   }
+  //   default: {
+  //     return Promise.reject(error);
+  //   }
+  // }
+}
+
+export const axios = Axios.create({
+  baseURL: ''
+})
+
+// 请求拦截
+axios.interceptors.request.use((config) => {
+  if (config && config.headers) {
+    config.headers.Accept = 'application/json'
+  }
+
+  return config
+})
+
+// 响应拦截
+axios.interceptors.response.use(
+  (response) => {
+    return response.data
+  },
+  (error) => {
+    return handleHttpCode(error)
+  }
+)
